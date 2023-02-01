d219851e8e7e4eff3ccfe63cd85b17b52b2994ed
Tetiana <shahdi007@gmail.com>
2023-02-01 16:34:05 -0500

    final commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PicturePageComponent } from './picture-page/picture-page.component';
 import { GalleryComponent } from './gallery/gallery.component';
-import { PictureResolver } from './picture-page/picture-resolver';
 
 const routes: Routes = [
   {path: '', component: GalleryComponent},
--- src/app/picture-page/picture-page.component.ts
+++ src/app/picture-page/picture-page.component.ts
@@ -1,38 +1,24 @@
-import { Component, Injectable, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PicturesService } from '../picture/PicturesService';
 import { Picture } from "../picture/pictures";
-import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { getLocalePluralCase } from '@angular/common';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
 @Component({
   selector: 'app-picture-page',
   templateUrl: './picture-page.component.html',
   styleUrls: ['./picture-page.component.css'],
   providers: [PicturesService]
 })
-export class PicturePageComponent  implements OnInit{
-  pictureInfo:Picture[]
-  pictureSubs!: Subscription;
+export class PicturePageComponent {
   temp:any
   id: any;
-  private querySubscription: Subscription;
+  private querySubscription: Subscription | undefined;
   constructor(private route: ActivatedRoute, private picturesService: PicturesService){
-    this.querySubscription = route.queryParams.subscribe(
+    this.querySubscription = this.route.queryParams.subscribe(
       (queryParam: any) => {
-          this.id = queryParam['id'];
-          console.log(this.id);   
+          this.temp = this.picturesService.getById(queryParam['id']); 
       }
-  );
-    this.pictureInfo = picturesService.getPicture();
-    this.temp = this.picturesService.getById(this.id);
-  }
-
-  ngOnInit(): void {
-
+    );
   }
 }
